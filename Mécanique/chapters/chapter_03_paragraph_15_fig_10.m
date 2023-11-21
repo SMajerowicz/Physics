025a2d786447a89c5ecb71777e2304d09426d469
SMajerowicz <majerow@yahoo.fr>
2023-11-21 21:00:28 +0100

    Begin paragraph15

---
+++ Mécanique/chapters/chapter_03_paragraph_15_fig_10.m
@@ -0,0 +1,9 @@
+r = 0:0.01:2.5;
+for alpha = 1:1:5
+  plot (r, - 1 * alpha ./ x + 1 ./ (2*power(r,2)));
+  text(0.5 + (alpha-1) ./ 3, -10 + (alpha-1),strcat('alpha=',num2str(alpha)));
+  hold on;
+endfor
+axis([0, 2.5, -14, 25]);
+xlabel ("r");
+ylabel ("U(r)");
