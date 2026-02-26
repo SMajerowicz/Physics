25d4b66c24d0dea73211b958601f48468e5e83b3
SMajerowicz <majerow@yahoo.fr>
2026-02-26 22:28:01 +0100

    Paragraph 29 on going

--- Mécanique/chapters/chapter_05_paragraph_29.m
+++ Mécanique/chapters/chapter_05_paragraph_29.m
@@ -1,4 +1,4 @@
-epsilon=0.0:0.001:0.5;
+epsilon=-2:0.001:2;
 lambda = 0.01;
 xi = 0.05;
 m = 0.25;
@@ -6,14 +6,14 @@ omega0 = 0.1;
 
 aux=figure();
 
-for f = 0.0:0.001:0.01
-  b2 = (epsilon .* epsilon + lambda .* lambda + sqrt(epsilon .* epsilon + lambda .* lambda - 2.0 .* xi .* f .* f .* epsilon ./ m ./ m ./ omega0 ./ omega0)) ./ 4.0 ./ xi ./ epsilon;
+for f = 0.001:0.01:0.1
+  b2 = ((epsilon .* epsilon + lambda .* lambda) - sqrt(epsilon .* epsilon + lambda .* lambda - 2.0 .* xi .* f .* f .* epsilon ./ m ./ m ./ omega0 ./ omega0)) ./ 4.0 ./ xi ./ epsilon;
 
   plot (epsilon,sqrt(b2));
   hold on;
 endfor
-axis([0,0.1,1.6,5]);
+axis([-1.5,2,-0.5,7.5]);
 xlabel("\\epsilon");
-ylabel("Amplitude^2");
+ylabel("b^2");
 
 saveas(aux, "./graphics/chapter_05_paragraph_29", "png");
